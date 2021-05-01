5f09add17e5e410453b1ff3d7762a8acbef213b7
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2021-05-01 10:00:07 +0800

    updated
    updates

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+'use strict';
+
 const entryForm = document.getElementById("entryForm");
+const entriesSection = document.getElementById("entries");
+const entryTextbox = document.getElementsByClassName("entry-textbox");
+
+
 
 function addEntryToDom(event) {
     event.preventDefault();
+    console.log(entryTextbox[0].value)
+    const entryDiv = document.createElement("div")
 }
 
-entryForm.addEventListener("submit", addEntryToDom)
+entryForm.addEventListener("submit", addEntryToDom)   
+
